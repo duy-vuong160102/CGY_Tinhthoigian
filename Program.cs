8e29665f2af79192362b5396a45c1f00dd6eeed9
duy-vuong160102 <duyvuong1601@gmail.com>
2023-07-06 00:08:27 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGY_Tinhthoigian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Datetime now: " + localDate);
+        }
+    }
+}
